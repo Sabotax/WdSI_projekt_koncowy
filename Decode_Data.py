284830a127b097f3a2598b52abcdc71d07af6b2e
Daniel <daniel.r5@tlen.pl>
2022-02-14 00:27:34 +0100

    change organization

--- Decode_Data.py
+++ Decode_Data.py
@@ -1,7 +1,8 @@
-# import os
+import os
 # import random
 import numpy as np
 import cv2
+import json
 # from sklearn.ensemble import RandomForestClassifier
 # import pandas
 
@@ -9,45 +10,81 @@ from xml.dom import minidom
 from dataclasses import dataclass
 
 class Decode_Data:
+    path_anno = "annotations\\"
+    path_images = "images\\"
+    DataUnit_list = []
 
-    def read_annotation(self,path,name):
-        full_path = path+name+".xml";
-        doc = minidom.parse(full_path)
-        
-        width = doc.getElementsByTagName('width')[0].childNodes[0].data
-        height = doc.getElementsByTagName('height')[0].childNodes[0].data
-        class_name = doc.getElementsByTagName('name')[0].childNodes[0].data
-        xmin = doc.getElementsByTagName('xmin')[0].childNodes[0].data
-        xmax = doc.getElementsByTagName('xmax')[0].childNodes[0].data
-        ymin = doc.getElementsByTagName('ymin')[0].childNodes[0].data
-        ymax = doc.getElementsByTagName('ymax')[0].childNodes[0].data
-        
-        return DataUnit(name, width, height, xmin, xmax, ymin, ymax, class_name,None)
-    
-    def read_image(self,ob):
-        ob.image = cv2.imread("images\\"+ob.filename+".png")
+    def generate_list(self):
+        onlyfiles = [f for f in os.listdir(self.path_anno) if os.path.isfile(os.path.join(self.path_anno, f))]
+        for i in range(len(onlyfiles)):
+            onlyfiles[i] = onlyfiles[i][0:len(onlyfiles[i])-4]
+        self.names_list = onlyfiles # ( ͡° ͜ʖ ͡°)
+
+    def fill_object_list(self):
+        for name in self.names_list:
+            self.DataUnit_list.append( DataUnit(name,self.path_anno,self.path_images) )
+
+        # testowanie
+        # self.DataUnit_list.append(DataUnit("road0", self.path_anno, self.path_images))
+        # self.DataUnit_list.append(DataUnit("road1", self.path_anno, self.path_images))
+        # self.DataUnit_list.append(DataUnit("road2", self.path_anno, self.path_images))
         print()
-    
-    
-@dataclass
+
+    # def save_DataUnit_list(self):
+    #     str = json.dumps(self.DataUnit_list[0].__dict__,skipkeys=True)
+    #     print(str)
+
 class DataUnit:
-    filename: str
-    width: int 
-    height: int 
-    box_xmin: int
-    box_xmax: int
-    box_ymin: int
-    box_ymax: int
-    class_name: str
-    image: np.array
+
+    def __init__(self,filename,path_anno,path_images):
+        self.name = filename
+        self.path_anno = path_anno
+        self.path_images = path_images
+
+        self.read_annotation()
+        self.read_image()
+
+    def read_annotation(self):
+        full_path = self.path_anno + self.name + ".xml"
+        doc = minidom.parse(full_path)
+
+        self.width = int(doc.getElementsByTagName('width')[0].childNodes[0].data)
+        self.height = int(doc.getElementsByTagName('height')[0].childNodes[0].data)
+        self.class_name = doc.getElementsByTagName('name')[0].childNodes[0].data
+        self.xmin = int(doc.getElementsByTagName('xmin')[0].childNodes[0].data)
+        self.xmax = int(doc.getElementsByTagName('xmax')[0].childNodes[0].data)
+        self.ymin = int(doc.getElementsByTagName('ymin')[0].childNodes[0].data)
+        self.ymax = int(doc.getElementsByTagName('ymax')[0].childNodes[0].data)
+
+    def read_image(self):
+        img = cv2.imread(self.path_images+self.name+".png")
+        cropped_img = img[self.ymin:self.ymax,self.xmin:self.xmax]
+        self.image = cropped_img
+        #self.image_to_list = self.image.tolist()
+
+
+
+
+# TODO zapisac liste obiektow wraz z zdekodowanymi obrazami do pliku wtedy to jest koniec w tym pliku .py (opcjonalnie)
+# nastepnie w nastepnym ogarnac skrypt do dzielenia na train i test
+
    
     
 
-dekoder1 = Decode_Data();
+#dekoder1 = Decode_Data();
 path = "annotations\\"
 filename = "road0"
-test = dekoder1.read_annotation(path,filename)
-dekoder1.read_image(test)
+#test = dekoder1.read_annotation(path,filename)
+# test = DataUnit("road0","annotations\\","images\\")
+# test.read_annotation()
+# test.read_image()
+# cv2.imshow("a",test.image)
+# cv2.waitKey(0)
+
+dekoder1 = Decode_Data()
+dekoder1.generate_list()
+dekoder1.fill_object_list()
+#dekoder1.save_DataUnit_list()
 print()
 
 
